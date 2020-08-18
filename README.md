d8d955a8e7e3c61eceb68596ec7853d766aad124
sleeperninja <mikeawatson6@gmail.com>
2020-08-18 16:23:58 -0400

    initial commit, script works (for me)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # YoutubeRipping
-PowerShell script to simplify Youtube-DL ripping
+
+*PowerShell script to simplify Youtube-DL ripping*
+
+## Get-YoutubeAudio
+
+Get-YoutubeAudio uses Youtube-DL, bound to environment variable $env:youtube, to download the highest quality audio from a specified Youtube URL, then adjust the volume so the average DR sits at -16dB using ConvertTo-LeveledALAC. The early version has practically no error checking, so you should only use it if you know what you're doing. 
+
+## Get-YoutubeVideo
+
+Get-YoutubeVideo uses Youtube-DL, bound to environment variable $env:youtube, to download the highest quality audio/video from a specified Youtube URL. 
+
+No error checking, no extra fun stuff just yet.
---
+++ YoutubeRipping.ps1
@@ -0,0 +1,17 @@
+﻿#audio
+function Get-YoutubeAudio ($urls, $outPath = "G:\Downloads\youtubeRips\audio"){ 
+    foreach ($url in $urls) {
+        $arg = "$($url) --get-title"
+        $title = (Invoke-Process $env:youtube -ArgumentList $arg).split([Environment]::NewLine)[0].replace('[','(').replace(']',')')
+        Start-Process $env:youtube -ArgumentList $url, "--extract-audio --audio-format best", '-o', """$($outPath)\$($title)`.%(ext)s""" -NoNewWindow -PassThru -Wait
+        $fullTitle = (Get-ChildItem $outPath | Where-Object {$_.Name -like "*$title*"}).FullName
+        ConvertTo-LeveledALAC -filePath $fullTitle -noLog
+    }
+}
+
+#video
+function Get-YoutubeVideo ($urls,$outPath = "g:\downloads\youtubeRips\video\%(title)s.%(ext)s"){
+    foreach ($url in $urls) {
+        Start-Process $env:youtube -ArgumentList $url, "-f bestvideo+bestaudio", '-o', $outPath -NoNewWindow -Wait -PassThru
+    }
+}
